a47cbe1e3e6e86053004e316e5be57906c834a99
hitalo <hitalocunhadesousa@gmail.com>
2017-10-05 01:38:10 -0300

    Organizando

---
+++ app/src/main/java/com/example/aluno/applogin/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.aluno.applogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usuario, senha;
+    Button botao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usuario = (EditText) findViewById(R.id.edtUsuario);
+        senha = (EditText) findViewById(R.id.edtSenha);
+        botao = (Button) findViewById(R.id.btnEntrar);
+
+    }
+
+    void logar(View v){
+        if (usuario.getText().toString().equals("admin") && senha.getText().toString().equals("1234567")){
+            Toast.makeText(this,"Senha correta", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Senha errada", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
