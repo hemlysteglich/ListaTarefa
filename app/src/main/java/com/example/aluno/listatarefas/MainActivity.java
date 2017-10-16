d1c2b14cce684a230be2ef66dd429dad9d7cccc4
hitalo07 <hitalocunhadesousa@gmail.com>
2017-10-16 20:20:29 -0300

    Iniciando Interface

--- app/src/main/java/com/example/aluno/listatarefas/MainActivity.java
+++ app/src/main/java/com/example/aluno/listatarefas/MainActivity.java
@@ -9,27 +9,29 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText usuario, senha;
+    EditText usuario , senha;
     Button botao;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         usuario = (EditText) findViewById(R.id.edtUsuario);
         senha = (EditText) findViewById(R.id.edtSenha);
         botao = (Button) findViewById(R.id.btnEntrar);
-
-        logar(botao);
-
     }
 
-    public void logar(View v){
-        if (usuario.getText().toString().equals("admin") && senha.getText().toString().equals("1234567")){
-            Toast.makeText(this,"Senha correta", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this,"Senha errada", Toast.LENGTH_SHORT).show();
+    public void logar(View v)
+    {
+        if (usuario.getText().toString().equals("admin") &&
+                senha.getText().toString().equals("12345")){
+            Toast.makeText(this,"Senha correda",Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            Toast.makeText(this,"Senha errada",Toast.LENGTH_SHORT).show();
         }
     }
 }
