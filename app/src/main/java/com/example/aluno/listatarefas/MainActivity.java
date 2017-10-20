155383f4c2f772572e5be001a49dd48070d824de
hitalo07 <hitalocunhadesousa@gmail.com>
2017-10-20 13:34:41 -0300

    Finalizando a interface

--- app/src/main/java/com/example/aluno/listatarefas/MainActivity.java
+++ app/src/main/java/com/example/aluno/listatarefas/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.aluno.listatarefas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.aluno.listatarefas.Tasks.TaskActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,14 +27,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void logar(View v)
     {
-        if (usuario.getText().toString().equals("admin") &&
-                senha.getText().toString().equals("12345")){
-            Toast.makeText(this,"Senha correda",Toast.LENGTH_SHORT).show();
-
+        if (usuario.getText().toString().equals("admin") && senha.getText().toString().equals("12345")){
+            Toast.makeText(this,"Login realizado com sucesso!",Toast.LENGTH_SHORT).show();
+            Intent i = new Intent();
+            i.setClass(this, TaskActivity.class);
+            startActivity(i);
         }
         else
         {
-            Toast.makeText(this,"Senha errada",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Senha incorreta",Toast.LENGTH_SHORT).show();
         }
     }
 }
---
+++ app/src/main/java/com/example/aluno/listatarefas/Tasks/NewTaskActivity.java
@@ -0,0 +1,15 @@
+package com.example.aluno.listatarefas.Tasks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aluno.listatarefas.R;
+
+public class NewTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+    }
+}
